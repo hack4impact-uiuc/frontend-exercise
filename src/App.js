c50345aa34b77159a28c9315b064f30b0a987218
Nithin Rajkumar <nithinrajkumar@wirelessprv-10-194-183-96.near.illinois.edu>
2018-02-16 17:17:39 -0600

    part 1 done

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>Hello World!</h1>
-        <Instructions />
+        <h1>This is Nithin's porn site!</h1>
+        <Instructions shouldDisplayImage />
       </div>
     )
   }
